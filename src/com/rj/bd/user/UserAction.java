f957a6e458bb1f90fe1793943803e08b3cf10c6d
zhaoyongchao666 <1620397804@qq.com>
2020-04-09 08:57:11 +0800

    提交代码到github远程仓库中  更改了哦

---
+++ src/com/rj/bd/user/UserAction.java
@@ -0,0 +1,11 @@
+package com.rj.bd.user;
+/** 
+* @author 作者 :赵泳钞 
+* @version 创建时间：2020年4月9日 上午8:56:01 
+*/
+public class UserAction {
+
+	public void sayHello(){
+		System.out.println(" 你好啊 github！！ 提交成功了哦");
+	}
+}
